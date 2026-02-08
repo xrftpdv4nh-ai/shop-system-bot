e4c9a4ca1af788b58a24baff9f89e145b4ad3842
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 01:35:09 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,1 +1,24 @@
+const fs = require("fs");
+const path = require("path");
 
+const dbPath = path.join(__dirname, "..", "database", "guilds.json");
+
+function ensureDB() {
+  if (!fs.existsSync(dbPath)) {
+    fs.writeFileSync(dbPath, JSON.stringify({}, null, 2));
+  }
+}
+
+function readDB() {
+  ensureDB();
+  return JSON.parse(fs.readFileSync(dbPath, "utf8"));
+}
+
+function writeDB(data) {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+  readDB,
+  writeDB
+};
