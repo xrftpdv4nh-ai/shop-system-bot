6b0dcaec10b659bc269cca152b42862219f64f18
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 01:33:40 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+    if (!message.guild) return;
+    if (message.author.bot) return;
 
+    // هنا هنضيف بعدين:
+    // - setline here
+    // - إرسال صورة الخط
+    // - حماية السبام
+  }
+};
