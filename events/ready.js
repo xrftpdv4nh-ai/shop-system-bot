14b7fa9e3cc05925ed4414f8b416d26cf92e5481
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 01:33:19 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: "ready",
+  once: true,
+  execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
 
+    client.user.setPresence({
+      activities: [{ name: "System Shop | Setup" }],
+      status: "online"
+    });
+  }
+};
