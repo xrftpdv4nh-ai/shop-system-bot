ae9404cc5fc702680683df7c90a6b071ae017112
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 01:34:38 +0200

    Update permissions.js

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,1 +1,8 @@
+module.exports = function hasAdminAccess(member) {
+  if (!member) return false;
 
+  return (
+    member.permissions.has("Administrator") ||
+    member.permissions.has("ManageGuild")
+  );
+};
