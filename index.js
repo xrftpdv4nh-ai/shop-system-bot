bb439f9911c019e434ec85d55ec33e05e74a8588
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 01:37:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+// تحميل الأحداث تلقائيًا
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.TOKEN);
