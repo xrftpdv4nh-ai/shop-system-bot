5d9fee258d6a8f059df8b664c7c1d46b17f9eba4
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 02:42:59 +0200

    Update show.js

--- commands/slash/moderation/show.js
+++ commands/slash/moderation/show.js
@@ -1,11 +1,20 @@
-// show.js
 const { SlashCommandBuilder } = require("discord.js");
 const hasAdminAccess = require("../../../utils/permissions");
+
 module.exports = {
   data: new SlashCommandBuilder().setName("show").setDescription("إظهار الروم"),
-  async execute(i){
-    if(!hasAdminAccess(i.member)) return i.reply({content:"❌",ephemeral:true});
-    await i.channel.permissionOverwrites.edit(i.guild.id,{ ViewChannel:true });
-    i.reply("👁️ تم إظهار الروم");
+
+  async execute(interaction) {
+    if (!hasAdminAccess(interaction.member))
+      return interaction.reply({ content: "❌ صلاحيات غير كافية", ephemeral: true });
+
+    await interaction.deferReply({ ephemeral: true });
+
+    await interaction.channel.permissionOverwrites.edit(
+      interaction.guild.id,
+      { ViewChannel: true }
+    );
+
+    await interaction.editReply("👁️ تم إظهار الروم");
   }
 };
