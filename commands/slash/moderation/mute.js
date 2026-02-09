285460d2b209db2052ad11b38da4e9f66d90a61e
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-09 02:41:04 +0200

    Update mute.js

--- commands/slash/moderation/mute.js
+++ commands/slash/moderation/mute.js
@@ -1,13 +1,26 @@
-// mute.js
 const { SlashCommandBuilder } = require("discord.js");
 const hasAdminAccess = require("../../../utils/permissions");
+
 module.exports = {
-  data: new SlashCommandBuilder().setName("mute").setDescription("ميوت (24 ساعة)")
-    .addUserOption(o=>o.setName("user").setDescription("العضو").setRequired(true)),
-  async execute(i){
-    if(!hasAdminAccess(i.member)) return i.reply({content:"❌",ephemeral:true});
-    const m=await i.guild.members.fetch(i.options.getUser("user").id);
-    await m.timeout(24*60*60*1000);
-    i.reply("🔇 تم الميوت");
+  data: new SlashCommandBuilder()
+    .setName("mute")
+    .setDescription("ميوت 24 ساعة")
+    .addUserOption(o => o.setName("user").setDescription("العضو").setRequired(true)),
+
+  async execute(interaction) {
+    if (!hasAdminAccess(interaction.member))
+      return interaction.reply({ content: "❌ صلاحيات غير كافية", ephemeral: true });
+
+    await interaction.deferReply({ ephemeral: true });
+
+    try {
+      const m = await interaction.guild.members.fetch(
+        interaction.options.getUser("user").id
+      );
+      await m.timeout(24 * 60 * 60 * 1000);
+      await interaction.editReply("🔇 تم الميوت");
+    } catch {
+      await interaction.editReply("❌ فشل تنفيذ الميوت");
+    }
   }
 };
