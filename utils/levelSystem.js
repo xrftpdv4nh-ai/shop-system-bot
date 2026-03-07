8e802da902bf3d754b03fb0a85e57f2789ed83fc
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-07 16:45:18 +0200

    Update levelSystem.js

--- utils/levelSystem.js
+++ utils/levelSystem.js
@@ -2,17 +2,26 @@ function calculateLevel(xp) {
   return Math.floor(0.1 * Math.sqrt(xp)) + 1;
 }
 
+function getXpRequiredForLevel(level) {
+  return Math.pow((level - 1) / 0.1, 2);
+}
+
+function getXpRequiredForNextLevel(currentLevel) {
+  return Math.pow(currentLevel / 0.1, 2);
+}
+
 function calculateUsageScore(user) {
   return (
-    (user.messageCount || 0) * 1 +
-    (user.voiceMinutes || 0) * 2 +
-    (user.commandUsage || 0) * 3
+    (user.messageCount || 0) +
+    ((user.voiceMinutes || 0) * 2) +
+    ((user.commandUsage || 0) * 3)
   );
 }
 
 function calculateRankScore(user) {
   return (
-    (user.xp || 0) +
+    (user.messageXp || 0) +
+    (user.voiceXp || 0) +
     Math.floor((user.credits || 0) / 10) +
     ((user.voiceMinutes || 0) * 2) +
     ((user.commandUsage || 0) * 3) +
@@ -22,6 +31,8 @@ function calculateRankScore(user) {
 
 module.exports = {
   calculateLevel,
+  getXpRequiredForLevel,
+  getXpRequiredForNextLevel,
   calculateUsageScore,
   calculateRankScore
 };
