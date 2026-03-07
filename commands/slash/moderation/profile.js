239b413d94cb697d603c5a264704aa8470f8dc1d
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-07 19:31:06 +0200

    Update profile.js

--- commands/slash/moderation/profile.js
+++ commands/slash/moderation/profile.js
@@ -50,7 +50,7 @@ module.exports = {
     const profileBuffer = await createProfileCard({
       username: targetUser.username,
       avatarURL,
-      crowns: userData.crowns || 0,
+      crowns: userData.credit || 0,
       messageLevel: userData.messageLevel || 1,
       voiceLevel: userData.voiceLevel || 1,
       rank,
