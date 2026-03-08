d9c5a244d1328424d6bf40c405f9e9e72fb73af9
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-08 02:54:54 +0200

    Update GuildConfig.js

--- models/GuildConfig.js
+++ models/GuildConfig.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
+const commandSettingSchema = new mongoose.Schema({
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+
+  roles: {
+    type: [String],
+    default: []
+  },
+
+  channels: {
+    type: [String],
+    default: []
+  },
+
+  alias: {
+    type: String,
+    default: ""
+  }
+}, { _id: false });
+
 const guildConfigSchema = new mongoose.Schema({
   guildId: {
     type: String,
@@ -17,31 +39,12 @@ const guildConfigSchema = new mongoose.Schema({
     default: null
   },
 
-  disabledCommands: {
-    type: [String],
-    default: []
-  },
-
-  commandRoles: {
-    type: Map,
-    of: [String],
-    default: {}
-  },
-
-  commandChannels: {
-    type: Map,
-    of: [String],
-    default: {}
-  },
-
-  shortcuts: {
-    type: [
-      {
-        name: String,
-        content: String
-      }
-    ],
-    default: []
+  commandSettings: {
+    profile: { type: commandSettingSchema, default: () => ({}) },
+    crowns: { type: commandSettingSchema, default: () => ({}) },
+    daily: { type: commandSettingSchema, default: () => ({}) },
+    top: { type: commandSettingSchema, default: () => ({}) },
+    leaderboard: { type: commandSettingSchema, default: () => ({}) }
   }
 }, {
   timestamps: true
