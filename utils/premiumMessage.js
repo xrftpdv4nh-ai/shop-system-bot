e1c7a6c3934d945a51b4991f8f6427dbc301390e
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-08 21:28:18 +0200

    Create premiumMessage.js

---
+++ utils/premiumMessage.js
@@ -0,0 +1,7 @@
+function getPremiumMessage() {
+  return "هذه الميزة متاحة لمشتركي Premium فقط. اشترك من صفحة Premium في الداشبورد أو من السيرفر الرسمي.";
+}
+
+module.exports = {
+  getPremiumMessage
+};
